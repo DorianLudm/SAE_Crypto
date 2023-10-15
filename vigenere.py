2d0f9629597fe0ce9ef14711c95f5314c8b00842
Ludmann Dorian <122395205+DorianLudm@users.noreply.github.com>
2023-10-15 23:18:49 +0200

    Décodage vigenère

---
+++ vigenere.py
@@ -0,0 +1,46 @@
+def decalage(lettre:str):
+    ascii = ord(lettre)
+    if 65 <= ascii <= 90:
+        return ascii - 65
+    elif 97 <= ascii <= 122:
+        return ascii - 97
+    return None
+
+def calc_len_msg(msg: list[str]):
+    res = 0
+    for ligne in msg:
+        for lettre in ligne:
+            res += (65 <= ord(lettre) <= 90 or 97 <= ord(lettre) <= 122)
+    return res
+
+def decode_vigenere(msg: list[str], cle: str):
+    res = []
+    while calc_len_msg(msg) > len(cle):
+        cle = cle*2
+    indice_cle = 0
+    for ligne in msg:
+        res_ligne = ""
+        for lettre in ligne:
+            lettre_decodee = ord(lettre) - decalage(cle[indice_cle])
+            if 65 <= ord(lettre) <= 90:    # Si la lettre est une minuscule
+                if lettre_decodee < 65:
+                    lettre_decodee += 26
+            elif 97 <= ord(lettre) <= 122: # Si la lettre est une majuscule
+                if lettre_decodee < 97:
+                    lettre_decodee += 26
+            if 65 <= ord(lettre) <= 90 or 97 <= ord(lettre) <= 122:
+                res_ligne += chr(lettre_decodee)
+                indice_cle += 1
+            else:
+                res_ligne += lettre
+        res.append(res_ligne)
+    return res
+    
+message_code = ["AE IOW ZQBLXR WASIXQ WJR YKJ KGYUJAGY UU OXSLN TXRCUQYM",
+"IY IRCTQ HPNF RR RQBIIIGOFN XQ WTCEKK DQ OIH MHXDUDQW BAYNVUDQYM",
+"NR MRRPQD SU CXVMUQV HOHLWLQ CYT LRY GRQYMTRRY RPBMVXTVUES",
+"QF EXNFO UEHAMAEM RV MQEWPGR IRCTQ HTREOVRQ XE HUOYKIFGXXOA"]
+cle = "PANGRAMME"
+
+msg_decode = decode_vigenere(message_code, cle)
+print(msg_decode)
