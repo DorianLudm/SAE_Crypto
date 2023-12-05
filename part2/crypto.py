8ab777c0e246cd64c0a67cfdc5fdc27916d4c7ce
Ludmann Dorian <dorianludm7@gmail.com>
2023-12-05 17:56:57 +0100

    Cassage_astucieuxV2 [TOFIX]

--- part2/crypto.py
+++ part2/crypto.py
@@ -10,18 +10,37 @@ def cassage_brutal(message_chiffre, taille_cle1,taille_cle2):
  
 def cassage_astucieux(message_clair, message_chiffre):
     """Cassage astucieux de la clé"""
-    set_decoded = set_encoded = set()
-    for cle in range(2**10):
-        msg_crypted = encrypt(message_clair, cle)
-        if msg_crypted in set_decoded:
-            return cle
-        set_encoded.add(message_chiffre)
-        msg_decrypted = decrypt(message_chiffre, cle)
-        if msg_decrypted in set_encoded:
-            return cle
-        set_decoded.add(msg_decrypted)
- 
-    
+    dict_messageInTheMiddle = dict()
+    for cle in range(1,2**8):
+        #Passage de M à C1
+        msg_crypted = encrypt(cle, message_clair)
+        if msg_crypted in dict_messageInTheMiddle.keys():
+            if encrypt(dict_messageInTheMiddle[msg_crypted], message_clair) == decrypt(dict_messageInTheMiddle[msg_crypted], message_chiffre):
+                return (cle, dict_messageInTheMiddle[msg_crypted], msg_crypted)
+        dict_messageInTheMiddle[msg_crypted] = cle
+
+        #Passage de C2 à C1
+        msg_decrypted = decrypt(cle, message_chiffre)
+        if msg_decrypted in dict_messageInTheMiddle.keys():
+            if encrypt(dict_messageInTheMiddle[msg_decrypted], message_clair) == decrypt(dict_messageInTheMiddle[msg_decrypted], message_chiffre):
+                return (dict_messageInTheMiddle[msg_decrypted], cle, msg_decrypted)
+        dict_messageInTheMiddle[msg_decrypted] = cle
+        
+    return None
+
 # cassage_brutal(0b00010001,5,5)
-print("La clé est : ",cassage_brutal(0b00010001,5,5))    
-print(cassage_astucieux(0b00010001, 0b01011101))
+# print("Les clés sont : ",cassage_brutal(0b00010001,5,5))    
+
+msg = 0b00010001
+cle = 0b00001101
+msg_crypted1 = encrypt(cle, msg)
+cle2 = 0b00001111
+msg_crypted2 = encrypt(cle2, msg_crypted1)
+print(cle, cle2)
+print(msg, msg_crypted1, msg_crypted2)
+# print(decrypt(cle2, msg_crypted2))
+# print(msg_crypted1)
+# print()
+# print(decrypt(8, msg_crypted2))
+# print(encrypt(12, msg))
+print("Les clés sont : ",cassage_astucieux(msg, msg_crypted2))
